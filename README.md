4feee9f38ed83eb1af8deb097d175240b69de1a7
Luiza Alexandru <alexandru97luiza@yahoo.com>
2024-09-27 18:45:57 +0300

    Add your own path to save files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PaintApp
 A simple GUI application for painting using tkinter
+
+All you have to do is Create a Data.py file and write your own directory path in which you would like to save your drawings.
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PaintApplication import *
+from Data import *
 
 paint=Tk()
 paint.title("Paint App")
-paint_app=Paint(paint,"C:/Users/alexa/Desktop/Probleme_Curs/Paint_App/saved_images")
+paint_app=Paint(paint,path)
 paint.mainloop()
